8a462154c7b8f8b2451ac857953db9dfb8f0992a
Yohan Tancrez <yohantancrez@gmail.com>
2023-08-10 04:26:19 +0200

    moved comment

--- game-app/src/app/components/Scoreboard.tsx
+++ game-app/src/app/components/Scoreboard.tsx
@@ -12,8 +12,6 @@ export default function Scoreboard() {
         className="leading-xl flex h-20 w-[10rem] items-center justify-center overflow-hidden rounded-lg bg-[#0A2F12] px-3 py-2 text-xl text-[#8FFFA8] shadow-md sm:w-[25rem] sm:max-w-none sm:px-6 sm:py-3"
         onClick={() => setScoreBoardTypes(1)}
       >
-        {/* TODO: Create a Typography component */}
-
         {scoreboardType === 0 ? (
           <div className="flex w-full items-center justify-between">
             <span className="hidden sm:block">ROUND 1</span>
@@ -23,6 +21,7 @@ export default function Scoreboard() {
             <span className="hidden sm:block">00:60</span>
           </div>
         ) : (
+          // TODO: Create a Typography component
           <span
             className="w-auto animate-scoreboard bg-gradient-to-r from-[#8FFFA8] from-[17%] via-[#FFFB8F] via-[48%] to-[#B6DAFB] to-[81%] bg-clip-text text-center text-[2.5rem] leading-[2.5rem] text-transparent sm:text-[3.5rem] sm:leading-[3.5rem]"
             onAnimationEnd={() => setScoreBoardTypes(0)}
