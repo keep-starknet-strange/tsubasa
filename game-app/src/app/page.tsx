791a1bbb397863b422664c8e4470f6a66d7464c9
ayushtom <ayush@martiandao.xyz>
2023-08-12 16:01:36 +0530

    feat: position elements

--- game-app/src/app/page.tsx
+++ game-app/src/app/page.tsx
@@ -5,9 +5,19 @@ import Scoreboard from "./components/Scoreboard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-4">
-      <Scoreboard />
-      <ConnectButton />
-      <Gameboard />
+      <div className="flex w-full items-end justify-end">
+        <div className="p-2">
+          <ConnectButton />
+        </div>
+      </div>
+      <div className="absolute flex h-screen w-screen flex-col items-center justify-center">
+        <div className="w-full md:relative md:h-full">
+          <div className="z-10 m-2 mx-auto w-max md:absolute md:left-1/2 md:top-0 md:m-0 md:-translate-x-1/2">
+            <Scoreboard />
+          </div>
+          <Gameboard />
+        </div>
+      </div>
     </main>
   );
 }
