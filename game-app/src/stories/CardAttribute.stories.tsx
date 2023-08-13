337a22114044858889830f2d84e40d9000a469e4
Rémi Roycourt <remiroycourt@gmail.com>
2023-08-13 16:09:53 +0200

    feat(game-app): card attributes

---
+++ game-app/src/stories/CardAttribute.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CardAttribute from "../app/components/CardAttribute";
+
+const meta = {
+  title: "CardAttribute",
+  component: CardAttribute,
+  parameters: {},
+} satisfies Meta<typeof CardAttribute>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CardAttributeStory: Story = {
+  args: {
+    value: 3,
+    type: "stamina",
+    team: "yellow",
+    pending: false,
+    hurt: false,
+    bonus: false,
+  },
+};
