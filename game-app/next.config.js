dcf1a0f3210eed939c362c4819e061f122e60da7
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-08-15 17:48:03 +1000

    config

--- game-app/next.config.js
+++ game-app/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ...nextConfig,
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate", "supports-color": "supports-color" });
+      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" });
     }
 
     return config;
