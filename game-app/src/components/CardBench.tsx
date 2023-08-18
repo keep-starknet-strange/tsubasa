990300fdcda77443c6a9ee238a949233d6a691bf
ayushtom <ayush@martiandao.xyz>
2023-08-19 02:09:16 +0530

    init

---
+++ game-app/src/components/CardBench.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+export default function PlayerBench() {
+  return (
+    // TODO: Extract colors to the Tailwind theme once properly defined
+    <div className="rounded-b-xl bg-[#80D794] p-6 font-new-airport font-medium sm:p-8"></div>
+  );
+}
---
+++ game-app/src/components/CardPlaceholder.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+
+export default function PlayerPlaceholder() {
+  return (
+    // TODO: Extract colors to the Tailwind theme once properly defined
+      <div className="rounded-b-xl bg-[#80D794] p-6 font-new-airport font-medium sm:p-8">
+          
+    </div>
+  );
+}
