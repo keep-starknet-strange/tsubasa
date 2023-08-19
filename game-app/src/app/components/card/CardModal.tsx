4f2fb05858b04f477eec633a48e583e7c2bd41f8
Rémi Roycourt <remiroycourt@gmail.com>
2023-08-20 01:44:28 +0200

    feat: card overlay

--- game-app/src/app/components/card/CardModal.tsx
+++ game-app/src/app/components/card/CardModal.tsx
@@ -2,6 +2,7 @@
 
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import CardModalContent from "./CardModalContent";
 
 export default function CardModal() {
   const [open, setOpen] = useState(true);
@@ -31,55 +32,25 @@ export default function CardModal() {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <Dialog.Panel className="bg' h-full w-full transform overflow-auto text-left shadow-xl transition-all sm:my-8 sm:p-6">
-              <div className="absolute right-0 top-0 hidden bg-red pr-4 pt-4 sm:block">
-                <button
-                  type="button"
-                  className="text-gray-400 hover:text-gray-500 focus:ring-indigo-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
-                  onClick={() => setOpen(false)}
-                >
-                  <span className="sr-only">Close</span>
-                  {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
-                </button>
-              </div>
-              <div className="sm:flex sm:items-start">
-                <div className="bg-red-100 mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
-                  {/* <ExclamationTriangleIcon
-                      className="text-red-600 h-6 w-6"
-                      aria-hidden="true"
-                    /> */}
-                </div>
-                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-gray-900 text-base font-semibold leading-6"
+            <Dialog.Panel className="h-full w-full transform overflow-auto p-10 transition-all">
+              <div className="relative">
+                <div className="absolute right-0 top-0">
+                  <button
+                    type="button"
+                    className="focus:ring-indigo-500 rounded-md px-4 text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+                    onClick={() => setOpen(false)}
                   >
-                    Deactivate account
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-gray-500 text-sm">
-                      Are you sure you want to deactivate your account? All of
-                      your data will be permanently removed from our servers
-                      forever. This action cannot be undone.
-                    </p>
-                  </div>
+                    Close
+                    {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
+                  </button>
                 </div>
-              </div>
-              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="bg-red-600 hover:bg-red-500 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto"
-                  onClick={() => setOpen(false)}
-                >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  className="text-gray-900 ring-gray-300 hover:bg-gray-50 mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset sm:mt-0 sm:w-auto"
-                  onClick={() => setOpen(false)}
-                >
-                  Cancel
-                </button>
+                <CardModalContent
+                  dribble={1}
+                  name="Lau Xu"
+                  position="Forward"
+                  stamina={2}
+                  team="Cairo"
+                />
               </div>
             </Dialog.Panel>
           </Transition.Child>
--- game-app/src/app/components/card/CardModalContent.tsx
+++ game-app/src/app/components/card/CardModalContent.tsx
@@ -1,5 +1,80 @@
-const CardModalContent = () => {
-  return <div></div>;
+import classNames from "classnames";
+import Card from "./Card";
+import { CardSize } from "./types";
+import { getCardSizeClassnames } from "./utils";
+import { DribbleIcon, StaminaIcon } from "../icons";
+
+interface CardModalContentProps {
+  team: string;
+  position: string;
+  dribble: number;
+  stamina: number;
+  name: string;
+  size: CardSize;
+}
+
+const cardSize = "xl";
+
+const CardModalContent = ({
+  team,
+  position,
+  dribble,
+  stamina,
+  name,
+}: CardModalContentProps) => {
+  return (
+    <div className="flex  flex-col items-center justify-center text-white">
+      <Card
+        captain={false}
+        color="blue"
+        dribble={3}
+        energy={2}
+        kind="card"
+        size={cardSize}
+        stamina={1}
+        hover={false}
+        player="1"
+      />
+
+      <div className={"mt-10 w-[224px]"}>
+        <h1 className="text-center text-2xl font-bold leading-7">{name}</h1>
+
+        <div className="flex">
+          <div className="flex-1 text-left">Team</div>
+          <div className="flex-1 text-right font-bold">{team}</div>
+        </div>
+
+        <div className="flex">
+          <div className="flex-1 text-left">Position</div>
+          <div className="flex-1 text-right font-bold">{position}</div>
+        </div>
+
+        <div className="flex">
+          <div className="flex-1 text-left">
+            <div className="flex items-center">
+              <div className="h-5 w-5">
+                <DribbleIcon />
+              </div>
+              <div className="ml-1">Dribble</div>
+            </div>
+          </div>
+          <div className="flex-1 text-right font-bold">{dribble}</div>
+        </div>
+
+        <div className="flex">
+          <div className="flex-1 text-left">
+            <div className="flex items-center">
+              <div className="h-5 w-5">
+                <StaminaIcon />
+              </div>
+              <div className="ml-1">Stamina</div>
+            </div>
+          </div>
+          <div className="flex-1 text-right font-bold">{stamina}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardModalContent;
