0967048479f152a0bc30b011005da17e228d6018
ayushtom <ayush@martiandao.xyz>
2023-08-19 22:21:49 +0530

    fix: build failure, typescript issues

--- game-app/src/app/components/CardBench.tsx
+++ game-app/src/app/components/CardBench.tsx
@@ -13,7 +13,7 @@ export default function PlayerBench(props: Props) {
   const { playersInBench } = props;
   return (
     <div className="flex w-11/12 items-start justify-center rounded-xl bg-[#80D794] p-6">
-      {[...Array(CARDS_ALLOWED_IN_DECK)].map((_, index) => (
+      {Array.from({ length: CARDS_ALLOWED_IN_DECK }, (_, index) => (
         <PlayerPlaceholder key={index} id={`bench-${index}`}>
           {playersInBench[index] ? <Card data={playersInBench[index]} /> : null}
         </PlayerPlaceholder>
