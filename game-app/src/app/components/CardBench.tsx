e0cce67b4ef62aa949dab5de626b4360aa525ae6
ayushtom <ayush@martiandao.xyz>
2023-08-19 20:55:51 +0530

    feat: setup drag and drop logic

--- game-app/src/app/components/CardBench.tsx
+++ game-app/src/app/components/CardBench.tsx
@@ -3,33 +3,21 @@
 import PlayerPlaceholder from "./CardPlaceholder";
 import Card from "./Card";
 
-export default function PlayerBench() {
+interface Props {
+  playersInBench: { data: { id: string; name: string; position: string } }[];
+}
+
+const CARDS_ALLOWED_IN_DECK = 4;
+
+export default function PlayerBench(props: Props) {
+  const { playersInBench } = props;
   return (
-    <div className="flex items-start justify-center gap-2 rounded-xl bg-[#80D794] p-6">
-      <PlayerPlaceholder position="bench-1">
-        <Card
-          playerName="Ayush"
-          data={{ name: "ayush", position: "Goalkeeper" }}
-        />
-      </PlayerPlaceholder>
-      <PlayerPlaceholder position="bench-2">
-        <Card
-          playerName="Lucas"
-          data={{ name: "Lucas", position: "Forward" }}
-        />
-      </PlayerPlaceholder>
-      <PlayerPlaceholder position="bench-3">
-        <Card
-          playerName="Yohan"
-          data={{ name: "Yohan", position: "Midfielder" }}
-        />
-      </PlayerPlaceholder>
-      <PlayerPlaceholder position="bench-4">
-        <Card
-          playerName="Click"
-          data={{ name: "Click", position: "Defender" }}
-        />
-      </PlayerPlaceholder>
+    <div className="flex w-11/12 items-start justify-center rounded-xl bg-[#80D794] p-6">
+      {[...Array(CARDS_ALLOWED_IN_DECK)].map((_, index) => (
+        <PlayerPlaceholder key={index} id={`bench-${index}`}>
+          {playersInBench[index] ? <Card data={playersInBench[index]} /> : null}
+        </PlayerPlaceholder>
+      ))}
     </div>
   );
 }
--- game-app/src/app/page.tsx
+++ game-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core";
+import { DndContext, DragEndEvent, pointerWithin } from "@dnd-kit/core";
 import PlayerBench from "./components/CardBench";
 import ConnectButton from "./components/ConnectButton";
 import Gameboard from "./components/Gameboard/Gameboard";
@@ -8,14 +8,90 @@ import Scoreboard from "./components/Scoreboard";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  // store number of players in bench
+  const [playersInBench, setPlayersInBench] = useState([
+    {
+      id: "Ayush",
+      name: "Ayush",
+      position: "Goalkeeper",
+    },
+    {
+      id: "Lucas",
+      name: "Lucas",
+      position: "Forward",
+    },
+    {
+      id: "Yohan",
+      name: "Yohan",
+      position: "Midfielder",
+    },
+    {
+      id: "Click",
+      name: "Click",
+      position: "Defender",
+    },
+  ]);
+
+  // store map of players on gameboard
   const [playerPositions, setPlayerPositions] = useState({});
   const onDragEnd = (e: DragEndEvent) => {
-    setPlayerPositions({ [`${e.over?.id}`]: e.active.data.current });
+    if (!e.over) return;
+
+    // if player is placed on bench
+    if (e.over.id.toString().includes("bench")) {
+      setPlayersInBench((prev) => {
+        return [...prev, e.active.data.current];
+      });
+      setPlayerPositions((prev) => {
+        let prevPosition;
+        Object.keys(prev).forEach((eachItem) => {
+          if (
+            prev?.[eachItem as keyof typeof playerPositions]?.id ===
+            e?.active?.data?.current?.id
+          ) {
+            prevPosition = eachItem;
+          }
+        });
+
+        return {
+          ...prev,
+          [prevPosition as keyof typeof playerPositions]: null,
+        };
+      });
+      return;
+    }
+    // if player is not placed on bench
+
+    setPlayersInBench((prev) =>
+      prev.filter((eachItem) => eachItem.id !== e?.active?.data?.current?.id)
+    );
+
+    setPlayerPositions((prev) => {
+      let prevPosition;
+      Object.keys(prev).forEach((eachItem) => {
+        if (
+          prev?.[eachItem as keyof typeof playerPositions]?.id ===
+          e?.active?.data?.current?.id
+        ) {
+          prevPosition = eachItem;
+        }
+      });
+
+      if (prevPosition) {
+        return {
+          ...prev,
+          [`${e?.over?.id}`]: e.active.data.current,
+          [prevPosition as keyof typeof playerPositions]: null,
+        };
+      } else {
+        return {
+          ...prev,
+          [`${e?.over?.id}`]: e.active.data.current,
+        };
+      }
+    });
   };
 
-  useEffect(() => {
-    console.log({ playerPositions });
-  }, [playerPositions]);
   return (
     <main className="flex min-h-screen flex-col items-center gap-4">
       <div className="flex w-full items-end justify-end md:fixed">
@@ -24,14 +100,14 @@ export default function Home() {
         </div>
       </div>
       <div className="flex h-full w-screen flex-1 flex-col">
-        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+        <DndContext collisionDetection={pointerWithin} onDragEnd={onDragEnd}>
           <div className="my-auto h-full w-full flex-1 md:relative md:flex md:items-center md:justify-center ">
             <div className="z-10 m-2 mx-auto w-max md:absolute md:left-1/2 md:top-0 md:m-0 md:-translate-x-1/2">
               <Scoreboard />
             </div>
             <Gameboard playerPositions={playerPositions} />
             <div className="z-10 m-2 mx-auto w-max md:absolute md:bottom-0 md:left-1/2 md:m-0 md:-translate-x-1/2">
-              <PlayerBench />
+              <PlayerBench playersInBench={playersInBench} />
             </div>
           </div>
         </DndContext>
