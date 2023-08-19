120739ab36a93cf58078419f50c5687fe4c72964
ayushtom <ayush@martiandao.xyz>
2023-08-19 15:21:44 +0530

    fix: build issue

--- game-app/src/app/components/Card.tsx
+++ game-app/src/app/components/Card.tsx
@@ -4,16 +4,22 @@ import { useDraggable } from "@dnd-kit/core";
 import PlayerPlaceholder from "./CardPlaceholder";
 import { CSS } from "@dnd-kit/utilities";
 
-export default function Card() {
+interface Props {
+  playerName: string;
+  data: object;
+}
+
+export default function Card(props: Props) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: "unique-id",
-});
+    id: props.playerName,
+    data: props.data,
+  });
   const style = {
     transform: CSS.Transform.toString(transform),
   };
   return (
     <div ref={setNodeRef} {...listeners} {...attributes} style={style}>
-      <div className="h-12 w-12 bg-black">hey</div>
+      <div className="h-12 w-12 bg-black">{props.playerName}</div>
     </div>
   );
 }
