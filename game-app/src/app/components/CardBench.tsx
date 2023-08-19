98ebe94c7eb60f79aa094302accde3be4daa7df7
ayushtom <ayush@martiandao.xyz>
2023-08-19 05:40:16 +0530

    feat: add dnd base

--- game-app/src/app/components/CardBench.tsx
+++ game-app/src/app/components/CardBench.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useDraggable } from "@dnd-kit/core";
 import PlayerPlaceholder from "./CardPlaceholder";
+import Card from "./Card";
 
 export default function PlayerBench() {
   return (
     <div className="flex items-start justify-center gap-2 rounded-xl bg-[#80D794] p-6">
-      <PlayerPlaceholder />
+      <PlayerPlaceholder>
+        <Card />
+      </PlayerPlaceholder>
       <PlayerPlaceholder />
       <PlayerPlaceholder />
       <PlayerPlaceholder />
