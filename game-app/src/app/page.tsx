b4d54f14d010602b423984adb344e1e5f3c99a1a
ayushtom <ayush@martiandao.xyz>
2023-08-19 14:28:41 +0530

    fix: responsive mobile view

--- game-app/src/app/page.tsx
+++ game-app/src/app/page.tsx
@@ -7,6 +7,9 @@ import Gameboard from "./components/gameboard/Gameboard";
 import Scoreboard from "./components/Scoreboard";
 
 export default function Home() {
+  const onDragEnd = ({ over }) => {
+    console.log({ over });
+  };
   return (
     <main className="flex min-h-screen flex-col items-center gap-4">
       <div className="flex w-full items-end justify-end md:fixed">
@@ -15,10 +18,7 @@ export default function Home() {
         </div>
       </div>
       <div className="flex h-full w-screen flex-1 flex-col">
-        <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={(e) => console.log("hey", e)}
-        >
+        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
           <div className="my-auto h-full w-full flex-1 md:relative md:flex md:items-center md:justify-center ">
             <div className="z-10 m-2 mx-auto w-max md:absolute md:left-1/2 md:top-0 md:m-0 md:-translate-x-1/2">
               <Scoreboard />
