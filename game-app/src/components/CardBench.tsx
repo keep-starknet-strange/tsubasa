5570b69380b486e16b8a1c5b00bb6438c4d188fd
ayushtom <ayush@martiandao.xyz>
2023-08-19 15:21:44 +0530

    fix: build issue

--- game-app/src/components/CardBench.tsx
+++ game-app/src/components/CardBench.tsx
@@ -1,18 +1,35 @@
 "use client";
 
-import { useDraggable } from "@dnd-kit/core";
 import PlayerPlaceholder from "./CardPlaceholder";
 import Card from "./Card";
 
 export default function PlayerBench() {
   return (
     <div className="flex items-start justify-center gap-2 rounded-xl bg-[#80D794] p-6">
-      <PlayerPlaceholder>
-        <Card />
+      <PlayerPlaceholder position="bench-1">
+        <Card
+          playerName="Ayush"
+          data={{ name: "ayush", position: "Goalkeeper" }}
+        />
       </PlayerPlaceholder>
-      <PlayerPlaceholder />
-      <PlayerPlaceholder />
-      <PlayerPlaceholder />
+      <PlayerPlaceholder position="bench-2">
+        <Card
+          playerName="Lucas"
+          data={{ name: "Lucas", position: "Forward" }}
+        />
+      </PlayerPlaceholder>
+      <PlayerPlaceholder position="bench-3">
+        <Card
+          playerName="Yohan"
+          data={{ name: "Yohan", position: "Midfielder" }}
+        />
+      </PlayerPlaceholder>
+      <PlayerPlaceholder position="bench-4">
+        <Card
+          playerName="Click"
+          data={{ name: "Click", position: "Defender" }}
+        />
+      </PlayerPlaceholder>
     </div>
   );
 }
