d59765fc3f6e111c65a8a8ac4375425475e4460e
ayushtom <ayush@martiandao.xyz>
2023-08-20 02:43:06 +0530

    feat: add hover box shadow and cards desktop view

--- game-app/src/app/components/CardBench.tsx
+++ game-app/src/app/components/CardBench.tsx
@@ -1,21 +1,30 @@
 "use client";
 
+import { ExtendedCardProps } from "../page";
 import PlayerPlaceholder from "./CardPlaceholder";
-import Card from "./Card";
-
-interface Props {
-  playersInBench: { id: string; name: string; position: string }[];
-}
+import Card from "./card/Card";
+import Draggable from "./dnd/Draggable";
 
 const CARDS_ALLOWED_IN_DECK = 4;
+
+interface Props {
+  playersInBench: ExtendedCardProps[];
+}
 
 export default function PlayerBench(props: Props) {
   const { playersInBench } = props;
   return (
-    <div className="flex w-11/12 items-start justify-center rounded-xl bg-[#80D794] p-6">
+    <div className="flex w-11/12 items-start justify-center rounded-xl bg-[#80D794] p-2">
       {Array.from({ length: CARDS_ALLOWED_IN_DECK }, (_, index) => (
         <PlayerPlaceholder key={index} id={`bench-${index}`}>
-          {playersInBench[index] ? <Card data={playersInBench[index]} /> : null}
+          {playersInBench[index] ? (
+            <Draggable
+              id={playersInBench[index].id}
+              data={playersInBench[index]}
+            >
+              <Card {...playersInBench[index]} />
+            </Draggable>
+          ) : null}
         </PlayerPlaceholder>
       ))}
     </div>
