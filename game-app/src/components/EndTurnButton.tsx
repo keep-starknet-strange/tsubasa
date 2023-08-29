e32954a96bcaa278eb23c53395e1269354920050
Cheelax <thomas.belloc@gmail.com>
2023-08-29 09:22:19 +0200

    add tapp behaviour

--- game-app/src/components/EndTurnButton.tsx
+++ game-app/src/components/EndTurnButton.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { useState } from "react";
+
 interface EndTurnProps {
   isWaiting: boolean;
 }
 
 export default function EndTurnButton({ isWaiting = false }: EndTurnProps) {
+  const [isTapped, setIsTapped] = useState(false);
+
   return (
     <button
-      className="flex flex-row items-center rounded-full border border-green-600 bg-neon p-3 px-6  font-bold text-greenBlack drop-shadow-lg hover:bg-green-200"
+      className={`flex flex-row items-center rounded-full border border-green-600 p-3 px-6 font-bold text-greenBlack drop-shadow-lg ${
+        isTapped ? "bg-green-200" : "bg-neon"
+      } absolute bottom-0 md:relative md:hover:bg-green-200`}
+      onTouchStart={() => setIsTapped(true)}
+      onTouchEnd={() => setIsTapped(false)}
       onClick={() => {
         // end turn logic
       }}
