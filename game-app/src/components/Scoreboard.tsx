c69e47284c2b41a06abb90e000d2c7494b3be1af
ayushtom <ayush@martiandao.xyz>
2023-08-31 20:28:46 +0530

    chore: remove commented code

--- game-app/src/components/Scoreboard.tsx
+++ game-app/src/components/Scoreboard.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { useState } from "react";
-// import { useDojo } from "../DojoContext";
+import { useDojo } from "../DojoContext";
 
 export default function Scoreboard() {
   const [scoreboardType, setScoreBoardTypes] = useState(0);
 
-  // const {
-  //   setup: {
-  //     systemCalls: { create_game },
-  //   },
-  //   account: { account },
-  // } = useDojo();
+  const {
+    setup: {
+      systemCalls: { create_game },
+    },
+    account: { account },
+  } = useDojo();
 
-  // const player_2_address = "0x0000000";
+  const player_2_address = "0x0000000";
 
   return (
     <div className="rounded-b-xl bg-green-500 p-3 font-new-airport font-medium sm:p-8">
@@ -39,7 +39,12 @@ export default function Scoreboard() {
           </span>
         )}
       </button>
-      {/* <button onClick={() => void create_game({ account, player_2_address })} className="p-2 uppercase text-xl">start game</button> */}
+      <button
+        onClick={() => void create_game({ account, player_2_address })}
+        className="p-2 text-xl uppercase"
+      >
+        start game
+      </button>
     </div>
   );
 }
