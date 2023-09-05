eaa190dba9f595f88b1e47e23e7ad1bf353e8bc1
Yohan Tancrez <yohantancrez@gmail.com>
2023-09-05 06:40:52 +0200

    fixed animation and slowed it

--- game-app/tailwind.config.js
+++ game-app/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         xxs: "10px",
       },
       animation: {
-        scoreboard: "scoreboard 1s linear forwards 100ms",
+        scoreboard: "scoreboard 1.25s linear forwards 100ms",
       },
       boxShadow: {
         md: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
