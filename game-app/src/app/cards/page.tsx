31762e708b9f0aa57502fcb81a6555f6e637c94c
Cheelax <thomas.belloc@gmail.com>
2023-09-11 11:01:31 +0200

    header for page

--- game-app/src/app/cards/page.tsx
+++ game-app/src/app/cards/page.tsx
@@ -6,17 +6,22 @@ import PlayerCollection from "@/components/PlayerCollection";
 import { testcards } from "@/helpers/testCards";
 import { CardData, ExtendedCardProps } from "@/components/card/types";
 import PlayerDeck from "@/components/PlayerDeck";
+import GenericButton from "@/components/buttons/GenericButton";
 
 const CardsPage = () => {
   const [playerCollection, setPlayerCollection] =
     useState<ExtendedCardProps[]>(testcards);
+
+  const countCardsInDeck = () => {
+    console.log(Object.keys(cardPositions).length);
+    return Object.keys(cardPositions).length;
+  };
 
   const [cardPositions, setCardPositions] = useState<
     Record<string, ExtendedCardProps>
   >({});
 
   const handleAddToDeck = (cardDetails: CardData) => {
-    // Trouver le premier emplacement vide
     let slot: string = "";
     for (let i = 1; i <= 8; i++) {
       if (!cardPositions[`slot${i}`]) {
@@ -25,7 +30,6 @@ const CardsPage = () => {
       }
     }
 
-    // Si un emplacement est trouvé, ajoutez la carte à cet emplacement
     if (slot) {
       setCardPositions((prev) => ({
         ...prev,
@@ -39,6 +43,19 @@ const CardsPage = () => {
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center">
+        <div className="mt-2 flex w-5/6 justify-between text-lg">
+          <div className="border border-green-600 p-3 px-6 text-center font-bold text-greenBlack drop-shadow-lg md:rounded-full">
+            {countCardsInDeck()}/8{" "}
+          </div>
+
+          <GenericButton
+            onClick={() => {
+              console.log("login");
+            }}
+            label="Play"
+            customStyles="rounded-br-3xl"
+          />
+        </div>
         <div className="mt-auto flex flex-wrap">
           <PlayerDeck numSlots={8} cardPositions={cardPositions} />
         </div>
