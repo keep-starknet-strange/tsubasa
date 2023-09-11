3b8d51eb4fda9bd43e6c50fe6d36c61f2c026fb5
Cheelax <thomas.belloc@gmail.com>
2023-09-11 15:05:41 +0200

    fix stories

--- game-app/src/stories/CardModal.stories.tsx
+++ game-app/src/stories/CardModal.stories.tsx
@@ -1,5 +1,7 @@
+import Card from "@/components/card/Card";
 import CardModal from "../components/card/CardModal";
 import type { Meta, StoryObj } from "@storybook/react";
+import { ExtendedCardProps } from "@/components/card/types";
 
 const meta = {
   title: "Card/Modal",
@@ -12,6 +14,19 @@ type Story = StoryObj<typeof meta>;
 
 export const CardModalStory: Story = {
   args: {
-    open: true,
+    cardData: {
+      id: "1",
+      kind: "card",
+      size: "xl",
+      color: "blue",
+      hover: false,
+      captain: false,
+      dribble: 1,
+      defense: 2,
+      energy: 3,
+    },
+    onAddToDeck: (cardDetails: ExtendedCardProps) => {
+      console.log("Card added to deck:", cardDetails);
+    },
   },
 };
