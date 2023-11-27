685a5d5b26aae3368e882ac4d801b00a74961fda
Cheelax <thomas.belloc@gmail.com>
2023-11-27 12:20:24 +0100

    🔧 chore: Disable "@typescript-eslint/no-unsafe-argument" rule in .eslintrc.js
    Explanation: The commit disables the "@typescript-eslint/no-unsafe-argument" rule in the .eslintrc.js file located in the game-app directory. This rule is turned off to allow unsafe arguments in TypeScript code.

--- game-app/.eslintrc.js
+++ game-app/.eslintrc.js
@@ -15,6 +15,7 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-redundant-type-constituents": "off",
